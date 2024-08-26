8a88ed498cd1877cc7eeeba1b3badc45d96e5c5a
Fisehatsion Adisu Abute <fisehatsionadisu@gmail.com>
2024-08-26 13:13:55 +0300

    Add files via upload

---
+++ examples_on_function_definition.py
@@ -0,0 +1,62 @@
+#This script is written by email: fisehatsionadisu@gmail.com, github: fisehazion/github and LinkedIn: http://www.linkedin.com/in/fisehatsion-adisu-abute-929279208
+
+print("defining function for practice")
+print("area and perimeter ")
+
+def rec(length, width):
+      area = length * width
+      perimeter = 2*length + 2*width
+      return area, perimeter 
+x,y = rec(90,120)   
+print(x,y) 
+
+def profitable(d1,d2):
+      area = d1*d2
+      invest = area > 700
+      return invest
+buy = profitable(90,120)   
+print(buy)  
+
+def greet(name="guest"):
+      print("welcome", name)
+greet()    #welcome geust
+greet("John") #welcome John
+
+def greet():
+       print("Hello from function")  
+       print("Have a great day")
+#function call
+greet()   
+
+ #function definition 
+def personal_great(name):
+       print("Hello",name)   
+       print("Have a great day") 
+#function calls
+personal_great("Wubi") 
+personal_great("Ano")  
+  # doubling the given number
+def double(number):
+                   print(number*2)
+                   
+double(6)        
+
+print("Thank you! ")
+
+
+                   
+                   
+                   
+                   
+      
+                  
+                                          
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                    
